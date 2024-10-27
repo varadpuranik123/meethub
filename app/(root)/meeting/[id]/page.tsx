25818318647f7bb293a185b66ab6cefc7fb7fe0d
vdpop <varadpuranik123@gmail.com>
2024-10-27 17:37:44 +0530

    params changes

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -7,12 +7,13 @@ import { useGetCallById } from '@/hooks/useGetCallById';
 import { useUser } from '@clerk/nextjs'
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
 import React, { useState } from 'react'
+import { useParams } from 'next/navigation'
 
-const Meeting = ({ params: { id } }: { params: { id: string } }) => {
-
+const Meeting = () => {
   const { isLoaded } = useUser();
-
   const [isSetupComplete, setIsSetupComplete] = useState(false)
+  const params = useParams()
+  const id = params?.id as string
 
   const { call, isCallLoading } = useGetCallById(id);
 
